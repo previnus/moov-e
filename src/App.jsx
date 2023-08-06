085ef59d0f38253b986f86ed41147b18b7fbfdfe
Previn K Appadu <previnus@gmail.com>
2023-08-06 22:08:02 +0400

    added header, footer and configure vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { useState, useEffect } from 'react';
+
 const App = () => {
+
+  useEffect(() => {
+    console.log('react initiated');
+  }, []);
 
   return (
     <>
+    <h1 class="text-3xl font-bold underline text-[#b91c1c]">Hello MOOV-e</h1>
+    <Header />
+    <Footer />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from './assets/imgs/logo.png'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Hello This is the header</h1>
+    </div>
+  )
+}
+
+export default Header
