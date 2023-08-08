238b6708434fff6c67eb7e43f3d3602296637a94
Previn K Appadu <previnus@gmail.com>
2023-08-08 23:11:48 +0400

    added mobile navigation

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,9 +1,12 @@
 import { HamburgerIcon, StarIcon } from "@chakra-ui/icons";
-import { Box, Container, Flex, Spacer, Text, Image } from "@chakra-ui/react";
+import { Box, Container, Flex, Spacer, Text, Image, Button } from "@chakra-ui/react";
 import {NavLink, Link} from "react-router-dom"
-
+import {Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react'
+import { useDisclosure } from "@chakra-ui/react";
 
 const Header = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
     const navStyles = {
         padding: "20px",
         textTransform: "uppercase",
@@ -31,8 +34,21 @@ const Header = () => {
                     <Box sx={navStyles}><NavLink to="/contact">Contact</NavLink></Box>
                 </Flex>
                 <Flex display={{base: "flex", md:"none"}}>
-                    <HamburgerIcon fontSize="30px"/>
+                    <HamburgerIcon fontSize="30px" onClick={onOpen}/>
                 </Flex>
+                <Modal isOpen={isOpen} size="full" onClose={onClose} motionPreset='slideInBottom'>
+                    <ModalOverlay />
+                    <ModalContent backgroundColor="#232323">
+                    <ModalCloseButton fontSize="20px" />
+                    <ModalBody>
+                        <Box sx={navStyles}><NavLink to="/">Home</NavLink></Box>
+                        <Box sx={navStyles}><NavLink to="/trending">Trending</NavLink></Box>
+                        <Box sx={navStyles}><NavLink to="/whatsnew">What's New</NavLink></Box>
+                        <Box sx={navStyles}><NavLink to="/pricing">Pricing</NavLink></Box>
+                        <Box sx={navStyles}><NavLink to="/contact">Contact</NavLink></Box>
+                    </ModalBody>
+                    </ModalContent>
+                </Modal>
             </Flex> 
         </Container>
         </>
