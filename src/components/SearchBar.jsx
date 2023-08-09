c4ad4352181ebcef6fa8e83e1d919a44bc8a0d40
Previn K Appadu <previnus@gmail.com>
2023-08-10 01:09:04 +0400

    made some general css adjustments

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,9 +5,9 @@ const SearchBar = () => {
   return (
     <>
         <InputGroup borderRadius={5} size="sm">
-        <Input type="text" placeholder="Search Movie ..." border="1px solid #949494" />
+        <Input type="text" placeholder="Search Movie ..." border="1px solid white" />
         <InputRightElement>
-        <Search2Icon color="gray.600" />
+        <Search2Icon color="white" />
         </InputRightElement>
         </InputGroup>
     </>
