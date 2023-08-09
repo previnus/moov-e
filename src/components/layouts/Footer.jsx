13511103177fbfaf92e25ef7814865afe990847e
Previn K Appadu <previnus@gmail.com>
2023-08-09 22:28:50 +0400

    added new link to footer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,11 +1,12 @@
-import { Container, Text, Box } from "@chakra-ui/react";
+import { Container, Text, Box } from "@chakra-ui/react"
 
 const Footer = () => {
     return ( 
         <>
-            <Box backgroundColor="#252525">
-                <Container as="footer" maxWidth="1140px" padding="25px 0">
-                        <Text align="center">Copyright © 2023 | MooV-e</Text>
+            <Box backgroundColor="#252525" padding="25px 0">
+                <Container as="footer" maxWidth="1140px" display='flex' justifyContent="space-between">
+                    <Text>Copyright © 2023 | MooV-e</Text>
+                    <Text>Developed by <a href="https://previn.me" target="_blank">Previn K Appadu</a></Text>
                 </Container>
             </Box>
         </>
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -20,40 +20,40 @@ const Header = () => {
     return ( 
         <>
         <Box background="#252525" borderBottom="1px solid #111111">
-        <Container as="header" maxWidth="1140px" >
-           <Flex alignItems="center">
-                <Box margin="15px 0">
-                        <Link to="/">
-                            <Image maxWidth={{base: "150px", md:"250px"}} src="/assets/imgs/logo.png" />
-                        </Link>
-                </Box>
-                <Spacer />
-                <Flex display={{base: "none", md:"flex"}}>
-                    <Box sx={navStyles}><NavLink to="/">Home</NavLink></Box>
-                    <Box sx={navStyles}><NavLink to="/trending">Trending</NavLink></Box>
-                    <Box sx={navStyles}><NavLink to="/whatsnew">What's New</NavLink></Box>
-                    <Box sx={navStyles}><NavLink to="/pricing">Pricing</NavLink></Box>
-                    <Box sx={navStyles}><NavLink to="/contact">Contact</NavLink></Box>
-                </Flex>
-                <Flex display={{base: "flex", md:"none"}}>
-                    <HamburgerIcon fontSize="30px" onClick={onOpen}/>
-                </Flex>
-                
-                <Modal isOpen={isOpen} size="full" onClose={onClose} motionPreset='slideInBottom'>
-                    <ModalOverlay />
-                    <ModalContent backgroundColor="#232323">
-                    <ModalCloseButton fontSize="20px" />
-                    <ModalBody>
-                        <Box sx={navStyles}><NavLink to="/" onClick={onClose}>Home</NavLink></Box>
-                        <Box sx={navStyles} ><NavLink to="/trending" onClick={onClose}>Trending</NavLink></Box>
-                        <Box sx={navStyles}><NavLink to="/whatsnew" onClick={onClose}>What's New</NavLink></Box>
-                        <Box sx={navStyles}><NavLink to="/pricing" onClick={onClose}>Pricing</NavLink></Box>
-                        <Box sx={navStyles}><NavLink to="/contact" onClick={onClose}>Contact</NavLink></Box>
-                    </ModalBody>
-                    </ModalContent>
-                </Modal>
-            </Flex> 
-        </Container>
+            <Container as="header" maxWidth="1140px" >
+            <Flex alignItems="center">
+                    <Box margin="15px 0">
+                            <Link to="/">
+                                <Image maxWidth={{base: "150px", md:"250px"}} src="/assets/imgs/logo.png" />
+                            </Link>
+                    </Box>
+                    <Spacer />
+                    <Flex display={{base: "none", md:"flex"}}>
+                        <Box sx={navStyles}><NavLink to="/">Home</NavLink></Box>
+                        <Box sx={navStyles}><NavLink to="/trending">Trending</NavLink></Box>
+                        <Box sx={navStyles}><NavLink to="/whatsnew">What's New</NavLink></Box>
+                        <Box sx={navStyles}><NavLink to="/pricing">Pricing</NavLink></Box>
+                        <Box sx={navStyles}><NavLink to="/contact">Contact</NavLink></Box>
+                    </Flex>
+                    <Flex display={{base: "flex", md:"none"}}>
+                        <HamburgerIcon fontSize="30px" onClick={onOpen}/>
+                    </Flex>
+                    
+                    <Modal isOpen={isOpen} size="full" onClose={onClose} motionPreset='slideInBottom'>
+                        <ModalOverlay />
+                        <ModalContent backgroundColor="#232323">
+                        <ModalCloseButton fontSize="20px" />
+                        <ModalBody>
+                            <Box sx={navStyles}><NavLink to="/" onClick={onClose}>Home</NavLink></Box>
+                            <Box sx={navStyles} ><NavLink to="/trending" onClick={onClose}>Trending</NavLink></Box>
+                            <Box sx={navStyles}><NavLink to="/whatsnew" onClick={onClose}>What's New</NavLink></Box>
+                            <Box sx={navStyles}><NavLink to="/pricing" onClick={onClose}>Pricing</NavLink></Box>
+                            <Box sx={navStyles}><NavLink to="/contact" onClick={onClose}>Contact</NavLink></Box>
+                        </ModalBody>
+                        </ModalContent>
+                    </Modal>
+                </Flex> 
+            </Container>
         </Box>
         
         </>
