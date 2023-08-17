2831107cbb69f2c4c0e4f341e0c0fb512bdf164e
Previn K Appadu <previnus@gmail.com>
2023-08-17 18:32:04 +0400

    cleaned up home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -21,37 +21,22 @@ const Home = () => {
     const fetchTopRated = async () => {
         const data = await fetch(urlTopRated);
         const movies = await data.json();
-        console.log(movies);
         setTopRated(movies.results);
     };
 
     const fetchNowPlaying = async () => {
         const data = await fetch(urlNowPlaying);
         const movies = await data.json();
-        console.log(movies);
         setNowPlaying(movies.results);
     };
 
     return ( 
         <Box backgroundColor="#000000">
 
-            <Carousel showThumbs={false} showStatus={false} autoPlay={true} infiniteLoop={true} interval={4000}>
+            <Carousel showThumbs={false} showStatus={false} autoPlay={true} infiniteLoop={true} interval={5000}>
                 {nowPlaying.map((movie) => {
                             return <Image key={movie.id} src={"https://image.tmdb.org/t/p/original" + movie.backdrop_path} fallbackSrc='https://via.placeholder.com/150'/>;
                             })}
-
-
-                <Box>
-                    <img src="https://source.unsplash.com/U6Q6zVDgmSs/1920x1080" />
-                </Box>
-                <Box>
-                    <img src="https://source.unsplash.com/U6Q6zVDgmSs/1920x1080" />
-                </Box>
-                <Box>
-                    <img src="https://source.unsplash.com/U6Q6zVDgmSs/1920x1080" />
-                </Box>
-
-
             </Carousel>
             <Box id="home-hero" padding="2rem">
 
