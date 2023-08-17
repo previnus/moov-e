d7131d38d0b4d342315a458259e174754b598384
Previn K Appadu <previnus@gmail.com>
2023-08-17 18:29:09 +0400

    added home page slider, top rated and now playing

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,22 +1,81 @@
-import { Container, Text, Heading, Box } from "@chakra-ui/react";
+import { Container, Text, Heading, Box, SimpleGrid, Image } from "@chakra-ui/react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { useState, useEffect } from "react";
+import MovieCard from "../MovieCard";
 
 const Home = () => {
-    const homeHeroStyles = {
-        backgroundImage: "https://source.unsplash.com/U6Q6zVDgmSs/1920x1080",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-        backgroundBlendMode: "darken",
-        backgroundPosition: "center center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-    }
+
+    const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
+    const baseUrl = import.meta.env.VITE_REACT_APP_BASE_URL;
+    const urlTopRated = `${baseUrl}/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`;
+    const urlNowPlaying = `${baseUrl}/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`;
+    const [topRated, setTopRated] = useState([]);
+    const [nowPlaying, setNowPlaying] = useState([]);
+
+    useEffect(() => {
+        fetchTopRated();
+        fetchNowPlaying();
+    },[]);
+
+    const fetchTopRated = async () => {
+        const data = await fetch(urlTopRated);
+        const movies = await data.json();
+        console.log(movies);
+        setTopRated(movies.results);
+    };
+
+    const fetchNowPlaying = async () => {
+        const data = await fetch(urlNowPlaying);
+        const movies = await data.json();
+        console.log(movies);
+        setNowPlaying(movies.results);
+    };
 
     return ( 
-        <Box id="home-hero" minHeight="75vh" sx={homeHeroStyles} display="flex">
-            <Container as="section" maxWidth={{ xl: "1140px"}}>
-                <Heading as="h1" fontSize="40px" fontWeight="600" align="center">HOME</Heading>
-                <Text as="h3" fontSize="25px" align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet minima ut, aspernatur voluptatem perferendis odio non quaerat animi provident tempora reprehenderit consequuntur, laborum asperiores veritatis numquam? Consectetur ducimus rerum nobis.</Text>
-            </Container>
+        <Box backgroundColor="#000000">
+
+            <Carousel showThumbs={false} showStatus={false} autoPlay={true} infiniteLoop={true} interval={4000}>
+                {nowPlaying.map((movie) => {
+                            return <Image key={movie.id} src={"https://image.tmdb.org/t/p/original" + movie.backdrop_path} fallbackSrc='https://via.placeholder.com/150'/>;
+                            })}
+
+
+                <Box>
+                    <img src="https://source.unsplash.com/U6Q6zVDgmSs/1920x1080" />
+                </Box>
+                <Box>
+                    <img src="https://source.unsplash.com/U6Q6zVDgmSs/1920x1080" />
+                </Box>
+                <Box>
+                    <img src="https://source.unsplash.com/U6Q6zVDgmSs/1920x1080" />
+                </Box>
+
+
+            </Carousel>
+            <Box id="home-hero" padding="2rem">
+
+                <Container as="section" maxWidth={{ xl: "1140px"}}>
+                    <Heading as="h1" fontSize="30px" fontWeight="600" align="Left" mb="1rem">Now Playing</Heading>
+                    <SimpleGrid spacing={4} columns={{sm: 1, md: 4}}>
+                        {nowPlaying.map((movie) => {
+                            return <MovieCard key={movie.id} movie={movie} />;
+                            })}
+                    </SimpleGrid>
+                </Container>
+                <Container as="section" maxWidth={{ xl: "1140px"}} mt="2rem">
+                    <Heading as="h1" fontSize="30px" fontWeight="600" align="left" mb="1rem">Top Rated</Heading>
+                    <SimpleGrid spacing={4} columns={{sm: 1, md: 4}}>
+                        {topRated.map((movie) => {
+                            return <MovieCard key={movie.id} movie={movie} />;
+                            })}
+                    </SimpleGrid>
+                </Container>
+
+            </Box>
+
         </Box>
+
      );
 }
  
