e3fc5f593037bf1f5b6be9b825ed6694578c2bd5
Previn K Appadu <previnus@gmail.com>
2023-08-18 02:41:55 +0400

    added responsive for single movie page

--- src/components/pages/Movie.jsx
+++ src/components/pages/Movie.jsx
@@ -1,4 +1,4 @@
-import { Container, Box, Heading, Text, Image, Flex } from "@chakra-ui/react";
+import { Container, Box, Heading, Text, Image, SimpleGrid } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
@@ -33,6 +33,7 @@ const Movie = () => {
     const movieHeroStyles = {
         backgroundImage: "https://image.tmdb.org/t/p/original" + movieData.backdrop_path,
         backgroundColor: "rgba(0, 0, 0, 0.5)",
+        //backgroundColor: 'linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0))';
         backgroundBlendMode: "darken",
         backgroundPosition: "center center",
         backgroundRepeat: "no-repeat",
@@ -45,20 +46,20 @@ const Movie = () => {
         <>
         <Box id="home-hero" minHeight="60vh" sx={movieHeroStyles} display="flex">
             <Container as="section" maxWidth={{ xl: "1140px"}}>
-                <Heading as="h1" fontSize="40px" fontWeight="600" align="center">{movieData.title}</Heading>
+                <Heading as="h1" fontSize="45px" fontWeight="800" align="center" color="#CB4335" textTransform="uppercase">{movieData.title}</Heading>
             </Container>
         </Box>
-        <Box backgroundColor="#000000">
+        <Box backgroundColor="#000000" padding="50px 0">
             <Container as="section" maxWidth={{ xl: "1140px"}}>
-                <Flex>
-                <Image src={"https://image.tmdb.org/t/p/w500" + movieData.backdrop_path} />
-                <Box padding="20px">
+                <SimpleGrid spacing={4} columns={{sm: 1, md: 2}}>
+                <Image src={"https://image.tmdb.org/t/p/w500" + movieData.backdrop_path} width="100%"/>
+                <Box>
                     <Heading as="h3">{movieData.title}</Heading>
-                    <Text>{movieData.overview}</Text>
-                    <Text>Release Date: {movieData.release_date}</Text>
+                    <Text mt="25px">{movieData.overview}</Text>
+                    <Text mt="25px">Release Date: {movieData.release_date}</Text>
                 </Box>
 
-                </Flex>
+                </SimpleGrid>
 
             </Container>
             
