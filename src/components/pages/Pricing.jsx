57316da3ede20dd7d606af79b376fb7a944f0279
Previn K Appadu <previnus@gmail.com>
2023-08-19 01:04:56 +0400

    added pricing page

--- src/components/pages/Pricing.jsx
+++ src/components/pages/Pricing.jsx
@@ -1,4 +1,6 @@
-import { Container, Box, Heading, Text } from "@chakra-ui/react";
+import { CheckCircleIcon, CheckIcon } from "@chakra-ui/icons";
+import { Container, Box, Heading, Text, SimpleGrid, List, ListItem, ListIcon, Flex, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const Pricing = () => {
     const pricingHeroStyles = {
@@ -10,13 +12,129 @@ const Pricing = () => {
         backgroundSize: "cover",
     }
 
+    const linkStyles = {
+        padding: "10px 20px",
+        bg: "#CB4335",
+        color: "#fff",
+        fontWeight: 700,
+        borderRadius: "10px",
+        ":hover": {
+            color: "#CB4335",
+            bg: "#fff"
+        }
+    }
+
     return ( 
-        <Box id="home-hero" minHeight="80vh" sx={pricingHeroStyles} display="flex">
+        <>
+        <Box id="home-hero" minHeight="50vh" sx={pricingHeroStyles} display="Flex">
             <Container as="section" maxWidth={{ xl: "1140px"}}>
                 <Heading as="h1" fontSize="40px" fontWeight="600" align="center">PRICING</Heading>
-                <Text as="h3" fontSize="25px" align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet minima ut, aspernatur voluptatem perferendis odio non quaerat animi provident tempora reprehenderit consequuntur, laborum asperiores veritatis numquam? Consectetur ducimus rerum nobis.</Text>
+                <Text as="h3" fontSize="25px" align="justify"></Text>
+                
             </Container>
+
         </Box>
+        <Box backgroundColor='#000' >
+            <Container as="section" maxWidth={{ xl: "1140px"}} paddingTop="50px" pb="50px">
+                <SimpleGrid spacing={4} columns={{sm: 1, md: 3}}>
+                    <Box width={{sm: "100%", md: "80%"}}  border="1px solid #fff" padding="25px" borderRadius="25px">
+                        <Heading as="h2" fontSize="20px" textAlign="center" mb="15px">BRONZE</Heading>
+                        <Heading as="h3" fontSize="16px" textAlign="center" mb="25px" color="#CB4335">Rs. 500 / Monthly</Heading>
+                        <hr />
+                        <Flex direction="column" mt="25px">
+                            <Box minHeight="200px">
+                                <List spacing={1} align="left" mb="25px">
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Standard Dashboard
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Save your favorite movie
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Rate your movie
+                                    </ListItem>
+                                </List> 
+                            </Box>
+ 
+                        <Link to="/contact"><Text sx={linkStyles}>Order</Text></Link>
+                        </Flex>
+                        
+                    </Box>
+                    <Box width={{sm: "100%", md: "80%"}} border="1px solid #fff" padding="25px" borderRadius="25px">
+                        <Heading as="h2" fontSize="20px" textAlign="center" mb="15px">SILVER</Heading>
+                        <Heading as="h3" fontSize="16px" textAlign="center" mb="25px" color="#CB4335">Rs. 1000 / Monthly</Heading>
+                    
+                        <hr />
+                        <Flex direction="column" mt="25px">
+                            <Box minHeight="200px">
+                                <List spacing={1} align="left" mb="25px">
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Premium Dashboard
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Save your favorite movie
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Buy Tickets Online
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Rate your movie
+                                    </ListItem>
+                                </List> 
+                            </Box>
+ 
+                        <Link to="/contact"><Text sx={linkStyles}>Order</Text></Link>
+                        </Flex>
+                        
+                    </Box>
+                    <Box width={{sm: "100%", md: "80%"}}  border="1px solid #fff" padding="25px" borderRadius="25px">
+                        <Heading as="h2" fontSize="20px" textAlign="center" mb="15px">GOLD</Heading>
+                        <Heading as="h3" fontSize="16px" textAlign="center" mb="25px" color="#CB4335">Rs. 1500 / Monthly</Heading>
+                    
+                        <hr />
+                        <Flex direction="column" mt="25px">
+                            <Box minHeight="200px">
+                                <List spacing={1} align="left" mb="25px">
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Premium Dashboard
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Save your favorite movie
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Rate your movie
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Buy Premium Ticket
+                                    </ListItem>
+                                    <ListItem>
+                                        <ListIcon as={CheckIcon} fontSize="12px" />
+                                        Get free goodies every month
+                                    </ListItem>
+                                </List> 
+                            </Box>
+ 
+                        <Link to="/contact"><Text sx={linkStyles}>Order</Text></Link>
+                        </Flex>
+                        
+                    </Box>
+                </SimpleGrid>
+                    
+            </Container>
+
+        </Box>
+        </>
      );
 }
  
